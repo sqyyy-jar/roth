98424115fbde2b15968640493090167a1811a12e
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-02-04 15:58:49 +0100

    Add tests

--- src/bytecode/src/lib.rs
+++ src/bytecode/src/lib.rs
@@ -1,13 +1,17 @@
+pub mod format;
 pub mod leb128;
 
 #[cfg(test)]
 mod test {
     use std::io::Cursor;
 
-    use crate::leb128::{decode, encode};
+    use crate::{
+        format::Binary,
+        leb128::{decode, encode},
+    };
 
     #[test]
-    fn test_encode() {
+    fn leb128_encode() {
         let a = 0xCAFEBABEu64;
         let mut bytes = Vec::with_capacity(9);
         encode(&mut bytes, a).expect("Encode to vec");
@@ -15,15 +19,15 @@ mod test {
     }
 
     #[test]
-    fn test_decode() {
+    fn leb128_decode() {
         const BYTES: [u8; 5] = [0xBE, 0xF5, 0xFA, 0xD7, 0x0C];
         let a = decode(&mut Cursor::new(&BYTES)).expect("Decode to u64");
         assert_eq!(a, 0xCAFEBABEu64);
     }
 
     #[test]
-    fn test_encode_max() {
-        let a = 0xFFFF_FFFF_FFFF_FFFFu64;
+    fn leb128_encode_max() {
+        let a = u64::MAX;
         let mut bytes = Vec::with_capacity(9);
         encode(&mut bytes, a).expect("Encode to vec");
         assert_eq!(
@@ -33,9 +37,41 @@ mod test {
     }
 
     #[test]
-    fn test_decode_max() {
+    fn leb128_decode_max() {
         const BYTES: [u8; 10] = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x01];
         let a = decode(&mut Cursor::new(&BYTES)).expect("Decode to u64");
-        assert_eq!(a, 0xFFFF_FFFF_FFFF_FFFFu64);
+        assert_eq!(a, u64::MAX);
+    }
+
+    #[test]
+    fn leb128_encode_min() {
+        let a = 0u64;
+        let mut bytes = Vec::with_capacity(9);
+        encode(&mut bytes, a).expect("Encode to vec");
+        assert_eq!(bytes, [0x00]);
+    }
+
+    #[test]
+    fn leb128_decode_min() {
+        const BYTES: [u8; 1] = [0x00];
+        let a = decode(&mut Cursor::new(&BYTES)).expect("Decode to u64");
+        assert_eq!(a, 0u64);
+    }
+
+    #[test]
+    fn format_encode_empty() {
+        let mut bytes = Vec::with_capacity(4);
+        let binary = Binary {
+            sections: Vec::with_capacity(0),
+        };
+        binary.write(&mut bytes).expect("Encode binary");
+        assert_eq!(bytes, [0x00, b'r', b't', b'h', 0x00])
+    }
+
+    #[test]
+    fn format_decode_empty() {
+        const BYTES: [u8; 5] = [0x00, b'r', b't', b'h', 0x00];
+        let binary = Binary::read(&mut Cursor::new(&BYTES)).expect("Decode binary");
+        assert_eq!(binary.sections.len(), 0);
     }
 }
